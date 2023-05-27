3b5c8611e0de9e53c1b2c15c88c33e199f76be87
Waffen <waffen.sultanofc@gmail.com>
2023-05-27 15:10:53 +0800

    Changed mode to production

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ import ViteExpress from 'vite-express'
 
 const app = express();
 
+// serve routes
 app.use(ViteExpress.static())
+
+// run app in production mode
+ViteExpress.config({ mode: 'production'})
 
 ViteExpress.listen(app, 5173, () => {
     console.log('Server is listening!')
